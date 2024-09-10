236c310cf58854dccf18daa6f17f6fe2a15ec629
joshjosey <156711944+joshjosey@users.noreply.github.com>
2024-09-10 00:21:03 -0500

    Update server.py
    formatted the response

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("__name__")
-
 #define the entrance functionality
 @app.route("/")
 def load_detector():
@@ -19,10 +18,13 @@ def emo_detector():
     #format the response
     response = "For the given statement, the system response is "
     #add in the emotions
-    response = response + "'anger': {emotions['anger']}"    
+    response = response + f"'anger': {emotions['anger']}, "  
+    response = response + f"'disgust': {emotions['disgust']}, "  
+    response = response + f"'fear': {emotions['fear']}, "  
+    response = response + f"'joy': {emotions['joy']}, "  
+    response = response + f"and 'sadness': {emotions['sadness']}. "    
+    response = response + f"The dominant emotion is <b>{emotions['dominant_emotion']}</b>"  
     return response
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
